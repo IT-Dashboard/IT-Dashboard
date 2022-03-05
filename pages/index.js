95c1b82b7a2a27cdcd2e28e0e059d3859f183a86
hooker.jacob <hooker4044@gamil.com>
2022-03-04 19:43:07 -0500

    Update page title to remove GTRI

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>GTRI IT Dashboard</title>
+        <title>IT Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
