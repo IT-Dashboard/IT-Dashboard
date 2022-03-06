4a22a81d80ddcf9f21f56e81549ee01dbd61e576
Robert Raheja <robertraheja@gmail.com>
2022-03-06 11:11:11 -0500

    Update example page with titles

--- pages/example1.js
+++ pages/example1.js
@@ -7,9 +7,15 @@ import {
   CardTitle,
 } from "reactstrap";
 
+import Head from "next/head";
+
 export default function Example1() {
   return (
     <div className="container">
+      <Head>
+        <title>Example Page | IT Dashboard</title>
+      </Head>
+      <h1>Example Page</h1>
       <div className="row align-items-start">
         <div className="col">
           <Card>
@@ -22,7 +28,7 @@ export default function Example1() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card&apos;s content.
               </CardText>
-              <Button>Button</Button>
+              <Button className="float-end">Button</Button>
             </CardBody>
           </Card>
         </div>
