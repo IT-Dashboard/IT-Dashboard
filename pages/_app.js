214e5a3895426a24e5e532ec9857a3ab667c7810
Robert Raheja <robertraheja@gmail.com>
2022-03-23 21:52:48 -0400

    Add layout and move dashboard sidebar/topbar to layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "../styles/index.css";
 
-
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page);
 
+  return getLayout(<Component {...pageProps} />);
+}
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,24 +1,12 @@
-import Dashboard from "../components/Dashboard";
-import Topbar from "../components/Topbar";
 import React, { useState } from "react";
-import Sidebar from "../components/Sidebar";
-
+import Layout from "../components/layout";
+import Dashboard from "../components/Dashboard";
 import Head from "next/head";
 
 export default function Page() {
-  const [showsidebar, setShowsidebar] = useState(true);
-  return (
-    <div className="min-h-screen">
-      <Head>
-        <title>Main Dashboard | IT Dashboard</title>
-      </Head>
-      {showsidebar && <Sidebar setShowsidebar={setShowsidebar} />}
-      <div>
-        <Topbar showsidebar={showsidebar} setShowsidebar={setShowsidebar} />
-      </div>
-      <div className={showsidebar ? " ml-0 lg:ml-60" : "ml-0"}>
-        <Dashboard />
-      </div>
-    </div>
-  );
+  return <Dashboard />;
 }
+
+Page.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
+};
