6ab34f700b1b2c4cdaaa15e844d8f746488766a1
Kyle Hoganson <hogansonkyle@gmail.com>
2022-04-07 18:08:43 -0400

    Adding create, delete, and update to api

--- pages/api/users.js
+++ pages/api/users.js
@@ -4,6 +4,9 @@ export default async function handler(req, res) {
   // let result = { env: process.env.DATABASE_URL };
 
   const result = await prisma.user.findMany();
+  const result = await prisma.user.create();
+  const result = await prisma.user.delete();
+  const result = await prisma.user.update();
 
   res.status(200).json(result);
 }
