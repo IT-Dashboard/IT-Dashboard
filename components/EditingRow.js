ce728a51f2f47883da688acf76909c817adaafb7
Robert Raheja <robertraheja@gmail.com>
2022-04-06 22:15:51 -0400

    Reformat EditingRow/ReadOnlyRow with prettier

--- components/EditingRow.js
+++ components/EditingRow.js
@@ -1,50 +1,79 @@
-import React from 'react'
+import React from "react";
 
-const EditingRow = ({ editFormData, handleEditFormChange, handleCancelClick }) => {
+const EditingRow = ({
+	editFormData,
+	handleEditFormChange,
+	handleCancelClick,
+}) => {
 	return (
-			<tr>
-				<td>
-					<input type="text" 
-					required="required" 
-					placeholder="Please enter your first name..." 
+		<tr>
+			<td>
+				<input
+					type="text"
+					required="required"
+					placeholder="Please enter your first name..."
 					name="firstName"
 					value={editFormData.firstName}
-					onChange={handleEditFormChange}></input>
-				</td>
-				<td><input type="text" 
-					required="required" 
-					placeholder="Please enter your last name..." 
+					onChange={handleEditFormChange}
+				></input>
+			</td>
+			<td>
+				<input
+					type="text"
+					required="required"
+					placeholder="Please enter your last name..."
 					name="lastName"
 					value={editFormData.lastName}
-					onChange={handleEditFormChange}></input>
-					</td>
-				<td><input type="text" 
-					required="required" 
-					placeholder="Please enter your address..." 
+					onChange={handleEditFormChange}
+				></input>
+			</td>
+			<td>
+				<input
+					type="text"
+					required="required"
+					placeholder="Please enter your address..."
 					name="address"
 					value={editFormData.address}
-					onChange={handleEditFormChange}></input>
-					</td>
-				<td><input type="text" 
-					required="required" 
-					placeholder="Please enter your phone number..." 
+					onChange={handleEditFormChange}
+				></input>
+			</td>
+			<td>
+				<input
+					type="text"
+					required="required"
+					placeholder="Please enter your phone number..."
 					name="phoneNumber"
 					value={editFormData.phoneNumber}
-					onChange={handleEditFormChange}></input>
-					</td>
-				<td><input type="text" 
-					required="required" 
-					placeholder="Please enter your email..." 
+					onChange={handleEditFormChange}
+				></input>
+			</td>
+			<td>
+				<input
+					type="text"
+					required="required"
+					placeholder="Please enter your email..."
 					name="email"
 					value={editFormData.email}
-					onChange={handleEditFormChange}></input>
-					</td>
-				<td class="inline-flex">
-					<button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-l" type="submit">Save</button>
-					<button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-r" type="button" onClick={handleCancelClick}>Cancel</button>
-					</td>
-			</tr>
-		)
-}
+					onChange={handleEditFormChange}
+				></input>
+			</td>
+			<td class="inline-flex">
+				<button
+					class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-l"
+					type="submit"
+				>
+					Save
+				</button>
+				<button
+					class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-r"
+					type="button"
+					onClick={handleCancelClick}
+				>
+					Cancel
+				</button>
+			</td>
+		</tr>
+	);
+};
 
-export default EditingRow
+export default EditingRow;
--- components/ReadOnlyRow.js
+++ components/ReadOnlyRow.js
@@ -1,36 +1,44 @@
-import React from 'react'
+import React from "react";
 
-const ReadOnlyRow = ({ user, index, handleEditClick, handleDeleteClick }) =>{
-	return (
-			<tr
-                      key={index}
-                      className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
-                    >
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {user.firstName}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {user.lastName}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {user.address}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {user.phoneNumber}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {user.email}
-                      </td>
-                      <td class="inline-flex">
-                        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-l" type="button" onClick={(event)=> handleEditClick(event, user)}>
-                        Edit 
-                        </button>
-                        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-r" type="button" onClick={()=> handleDeleteClick(user.id)}>
-                        Delete
-                        </button>
-                      </td>
-            </tr>
-		);
+const ReadOnlyRow = ({ user, index, handleEditClick, handleDeleteClick }) => {
+  return (
+    <tr
+      key={index}
+      className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+    >
+      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+        {user.firstName}
+      </td>
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {user.lastName}
+      </td>
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {user.address}
+      </td>
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {user.phoneNumber}
+      </td>
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {user.email}
+      </td>
+      <td class="inline-flex">
+        <button
+          class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-l"
+          type="button"
+          onClick={(event) => handleEditClick(event, user)}
+        >
+          Edit
+        </button>
+        <button
+          class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-r"
+          type="button"
+          onClick={() => handleDeleteClick(user.id)}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
 };
 
-export default ReadOnlyRow
+export default ReadOnlyRow;
