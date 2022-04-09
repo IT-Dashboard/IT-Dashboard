20c24f0ac3d8e0c5fad49f18cf04a752dc5ed467
Sky Kim <skynimelol@gmail.com>
2022-04-09 15:18:00 -0400

    Users page comments

--- pages/users.js
+++ pages/users.js
@@ -4,11 +4,16 @@ import ReadOnlyRow from "../components/ReadOnlyRow";
 import EditingRow from "../components/EditingRow";
 import prisma from "../lib/prisma";
 import { withSessionSsr } from "../lib/session";
+//nanoid generates id for the user
 import { nanoid } from "nanoid";
 import Link from "next/link";
 
 export default function Page({ users: initialUsers }) {
+
+  //initialize user data from database to use for table
   const [users, setUsers] = useState(initialUsers);
+
+  //store/initialize new user values as object in a state hook
   const [addFormData, setAddFormData] = useState({
     firstName: "",
     lastName: "",
@@ -17,6 +22,7 @@ export default function Page({ users: initialUsers }) {
     email: "",
   });
 
+  //create state object to hold form data while editing a row
   const [editFormData, setEditFormData] = useState({
     firstName: "",
     lastName: "",
@@ -25,20 +31,32 @@ export default function Page({ users: initialUsers }) {
     email: "",
   });
 
+  //if editUserId is null, the user is not editing any rows, hiding EditingRows component
   const [editUserId, setEditUserId] = useState(null);
 
+  //event handler function to update values of new user in a form
+  //form values saved in state
   const handleAddFormChange = (event) => {
     event.preventDefault();
 
+    //get name of input a user has changed
+    //fieldName becomes name of changed value (firstName, lastName, address, etc)
+    //fieldValue accepts the value of that variable
     const fieldName = event.target.getAttribute("name");
     const fieldValue = event.target.value;
 
+    //creates copy of the existing form data to change without changing state
     const newFormData = { ...addFormData };
+    //updates objects with the new data the user inputs
     newFormData[fieldName] = fieldValue;
 
+    //set into state and pass in new form data
     setAddFormData(newFormData);
   };
 
+
+  //updates state when any of the values change
+  //works the same way as handleAddFormChange
   const handleEditFormChange = (event) => {
     event.preventDefault();
 
@@ -48,13 +66,17 @@ export default function Page({ users: initialUsers }) {
     const newFormData = { ...editFormData };
     newFormData[fieldName] = fieldValue;
 
+    //set into state and pass in new form data
     setEditFormData(newFormData);
   };
 
+  //submits form of new user data
   const handleAddFormSubmit = (event) => {
     event.preventDefault();
 
+    //takes entered data in form(stored in state object) and create a new object
     const newUser = {
+      //id used to determine which user is being changed or updated
       id: nanoid(),
       firstName: addFormData.firstName,
       lastName: addFormData.lastName,
@@ -63,13 +85,17 @@ export default function Page({ users: initialUsers }) {
       email: addFormData.email,
     };
 
+    //create new user array to avoid changing state
     const newUsers = [...users, newUser];
+    //pass in new array
     setUsers(newUsers);
   };
 
+  //submits form of edited user data
   const handleEditFormSubmit = (event) => {
     event.preventDefault();
 
+    //new object created based on the inputs exiting in the editFormData
     const editedUser = {
       id: editUserId,
       firstName: editFormData.firstName,
@@ -79,20 +105,31 @@ export default function Page({ users: initialUsers }) {
       email: editFormData.email,
     };
 
+    //create new user array to avoid mutating state
     const newUsers = [...users];
 
+    //replace user object with new user object
+    //to do this get index of row that is being edited
     const index = users.findIndex((user) => user.id === editUserId);
 
+    //update array at the given index
     newUsers[index] = editedUser;
 
+    //set new array into state
     setUsers(newUsers);
+    //set EditUserId to null to hide EditingRow component
     setEditUserId(null);
   };
 
+  //event listener function triggered when edit button is clicked
+  //needs user to find the user id, and sets the EditUserId
+  //takes data from the user object and saves it to the editFormData to prepopulate row
   const handleEditClick = (event, user) => {
     event.preventDefault();
     setEditUserId(user.id);
 
+    //new object to hold same properties as editFormData
+    //used to prepopulate user data that is being edited
     const formValues = {
       firstName: user.firstName,
       lastName: user.lastName,
@@ -101,20 +138,28 @@ export default function Page({ users: initialUsers }) {
       email: user.email,
     };
 
+    //set into state with the current values
     setEditFormData(formValues);
   };
 
+  //reverts the EditUserId as null to render the ReadOnlyRow and unrender the EditingRow component
   const handleCancelClick = () => {
     setEditUserId(null);
   };
 
+  //Remove row from state, and rerender component with new array with the row removed
+  //userId used to find index of the user to remove that specific row
   const handleDeleteClick = (userId) => {
+
+    //create new array from current user to stop state from changing
     const newUsers = [...users];
 
+    //get index of user
     const index = users.findIndex((user) => user.id === userId);
 
+    //splice method use to remove user object at the given index of an array
     newUsers.splice(index, 1);
-
+    //save into state and pass in new array
     setUsers(newUsers);
   };
 
@@ -141,9 +186,19 @@ export default function Page({ users: initialUsers }) {
         <div className="overflow-x-auto">
           <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
             <div className="overflow-hidden">
+            //editing row component needs to be in a form tag but <form> cannot appear as a child of <tbody>
+            //to get around this problem, the whole table is wrapped in form tag
+            //works due to only having one set of form inputs displayed at any given time, preventing any form duplication problems
+            //implements the handleEditFormSubmit to submit changed made to editFormData
               <form onSubmit={handleEditFormSubmit}>
+
+              //start of table
                 <table className="min-w-full">
+
+                //column headings
                   <thead className="bg-white border-b bg-gray-50">
+
+                  //heading row
                     <tr>
                       <th
                         scope="col"
@@ -183,16 +238,30 @@ export default function Page({ users: initialUsers }) {
                       </th>
                     </tr>
                   </thead>
+                  //ending of column row
+
+                  //table body
                   <tbody>
                     {users.map((user, index) => (
+                      //react Fragment used due to not being able to have two children components
                       <Fragment key={index}>
+                        //edituserid checked to determine which component will be rendered
+                        //if id of current user id matches the id stored in state in the EditingUserId state hook,
+                        //it will render the EditingRow, if not it wil render ReadOnlyRow
                         {editUserId === user.id ? (
+                          //editFormData holds current values of a row and carries them over to EditingRows to be prepopulated
+                          //handleEditFormChange to update state onchange as well as the editFormData
+                          //handleCancelClick used to cancel an edit
                           <EditingRow
                             editFormData={editFormData}
                             handleEditFormChange={handleEditFormChange}
                             handleCancelClick={handleCancelClick}
                           />
                         ) : (
+                        //ReadOnlyRow component rendered
+                        //all the data needed for the component is passed in as a prop (ex. user)
+                        //handeEditClick passed in due to the edit button displayed on the ReadOnlyRow
+                        //handleDeleteClick passed in for delete button
                           <ReadOnlyRow
                             user={user}
                             handleEditClick={handleEditClick}
@@ -208,6 +277,9 @@ export default function Page({ users: initialUsers }) {
           </div>
         </div>
       </div>
+
+      //form to add new user
+      //updates form through "onChange" passing in new user event
       <form
         onSubmit={handleAddFormSubmit}
         className="mt-10 border p-9 bg-white shadow-md"
@@ -264,6 +336,8 @@ export default function Page({ users: initialUsers }) {
           className=" border px-2 py-2 mt-2 w-full rounded-sm"
         />
         <div className="flex items-center justify-between">
+
+        //submission button for adding user form
           <button
             type="submit"
             className=" mt-3 px-3 py-2 rounded-sm text-sm hover:bg-blue-800 bg-pr text-white"
