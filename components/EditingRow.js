3912bd5565e4fbdee73797f184ec4232e632edf5
Sky Kim <skynimelol@gmail.com>
2022-04-09 15:29:48 -0400

    editing row comment fixed comment error

--- components/EditingRow.js
+++ components/EditingRow.js
@@ -9,7 +9,7 @@ const EditingRow = ({
 }) => {
 	return (
 		//everytime there is an input, the input value is stored in state
-		//which is then used to repopulate the edit row
+		//which is then used to repopulate the edit row 
 		<tr>
 			<td>
 				<input
@@ -67,14 +67,16 @@ const EditingRow = ({
 				></input>
 			</td>
 			<td className="inline-flex">
-				//submit button to submit new edit
+			 //submit button to submit new edit 
 				<button
 					className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-l"
 					type="submit"
 				>
 					Save
 				</button>
-				//cancel button that triggers handleCancelClick
+				{/*
+			 	//cancel button that triggers handleCancelClick 
+				*/}
 				<button
 					className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-r"
 					type="button"
