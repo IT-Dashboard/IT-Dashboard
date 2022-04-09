b91b0272a56f3c195e28a18ce49aae97b3e867cd
Sky Kim <skynimelol@gmail.com>
2022-04-09 15:30:08 -0400

    readonlyrow comment fix error

--- components/ReadOnlyRow.js
+++ components/ReadOnlyRow.js
@@ -25,8 +25,10 @@ const ReadOnlyRow = ({ user, index, handleEditClick, handleDeleteClick }) => {
         {user.email}
       </td>
       <td className="inline-flex">
-      	//calls handleEditClick function to set EditUserId
+      	{/* 
+		//calls handleEditClick function to set EditUserId
 		// when edit button is clicked, it stores the id of user, which is used to show whether to show EditingRow or ReadOnlyRow component
+		*/}
         <button
           className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-l"
           type="button"
@@ -34,7 +36,7 @@ const ReadOnlyRow = ({ user, index, handleEditClick, handleDeleteClick }) => {
         >
           Edit
         </button>
-
+        
         //delete button
         <button
           className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-normal text-sm py-2 px-3 rounded-r"
