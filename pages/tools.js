8cbd8a15916af2a6f557704a6aecc36416405a6e
Robert Raheja <robertraheja@gmail.com>
2022-04-17 16:39:06 -0400

    Add placeholder pages for departments, organization, and tools

---
+++ pages/tools.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import Dashboard from "../components/Dashboard";
+import { withSessionSsr } from "../lib/session";
+
+export default function Page() {
+  return <h1 className="text-2xl">Tools in progress</h1>;
+}
+
+Page.getLayout = function getLayout(page) {
+  return <Layout title="Work in Progress">{page}</Layout>;
+};
+
+export const getServerSideProps = withSessionSsr(
+  async function getServerSideProps({ req }) {
+    const user = req.session.user;
+
+    if (!user) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        user: req.session.user,
+      },
+    };
+  }
+);
