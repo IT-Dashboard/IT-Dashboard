1cbeda45c728230391a684e376864a746e283bc9
Robert Raheja <robertraheja@gmail.com>
2022-04-17 16:55:55 -0400

    Remove unused link from footer and replace with copyright line

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,27 +4,11 @@ const Footer = () => {
   return (
     <div className=" absolute bottom-0 left-0 flex items-center justify-center w-full h-52 bg-gray-200">
       <div className="container">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 ">
           <div className="flex items-center justify-center flex-col gap-1">
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/">Home</Link>
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/">Signup</Link>
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/about">About</Link>
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/contact">Contact</Link>
+            &copy; 2022 Jacob Hooker, Kyle Hoganson, Robert Raheja, Sam Kharel,
+            Sky Kim
           </div>
-        
-          <div className="flex items-center justify-center flex-col gap-1">
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/">Dashboard</Link>
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/tool1">Tool 1</Link>
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/tool2">Tool 2</Link>
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/tool3">Tool 3</Link>
-          </div>
-        
-          <div className="flex items-center justify-center flex-col gap-1">
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/resources">Resource</Link>
-            <Link className=" font-medium text-gray-700 hover:text-pr" href="/guidelines">Guidelines</Link>
-           
-          </div>
-        
         </div>
       </div>
     </div>
